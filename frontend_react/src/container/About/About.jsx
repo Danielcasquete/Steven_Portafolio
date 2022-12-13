247b87835a86a2238dfc25f43d3d21a689ce856c
Daniel Steven <danicasquete02@gmail.com>
2022-12-12 22:08:33 -0500

    wrapper commit

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 
 import './About.scss';
 import { urlFor, client } from '../../client';
+import { AppWrap } from '../../wrapper';
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -40,4 +41,4 @@ const About = () => {
 };
 
 
-export default About
+export default AppWrap(About, 'about');
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {AiFillEve, AiFillGithub} from 'react-icons/ai';
+import {motion} from 'framer-motion';
+
+import {AppWrap} from '../../wrapper';
+import {urlFor, client} from '../../client';
+import './Work.scss'
 
 import './Work.scss';
 
+const [activeFilter, setactiveFilter] = useState('All')
+
 const Work = () => {
+
+const handleWorkFilter = (item) => {
+
+}
+
   return (
-    <div>Work</div>
-  )
+    <>
+      <h2 className="head-text">My creative <span>Portafolio</span> </h2>
+
+      {['UI/UX', 'Web App', 'Mobile App', 'All'].map((item, index) => (
+        <div
+        key={index}
+        onClick={() => handleWorkFilter(item)}
+        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+        >
+          
+        </div>
+      ))}
+
+    </>
+  ) 
 }
 
 export default Work
